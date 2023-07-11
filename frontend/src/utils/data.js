3dbf163524b7b064fe18927cafde2805b5a59217
Nataliia Gospodarikova <gospodarikovan@yandex.ru>
2023-07-11 22:34:49 +0300

    Fix frontend url

--- frontend/src/utils/data.js
+++ frontend/src/utils/data.js
@@ -5,8 +5,8 @@ export const validationConfig = {
   popupInputSelector: '.popup__input'
 }
 
-export const baseUrl = 'api.mesto-nataly.nomoredomains.work';
+export const baseUrl = 'https://api.mesto-nataly.nomoredomains.work';
 
-export const authBaseUrl = 'api.mesto-nataly.nomoredomains.work';
+export const authBaseUrl = 'https://api.mesto-nataly.nomoredomains.work';
 
 export const token = '1c01ae86-354c-49a1-92cb-37fce79fdf70';
